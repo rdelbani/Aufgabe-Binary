58c53214058afb9122b16223d57ed85f0eb73d2b
rdelbani <99690782+rdelbani@users.noreply.github.com>
2022-02-16 15:23:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 
 using namespace std;
@@ -16,7 +8,6 @@ using namespace std;
 #include<cmath>
 #include <tuple>
 #include <numeric>
-
 
 
 vector<int> create_array(int size){
